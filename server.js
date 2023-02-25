5621bc8b70a1b375e852530547238b8e4bc34fd3
Solomon Vana <117047239+Solomon-Coding@users.noreply.github.com>
2023-02-25 15:55:02 -0600

    Class version making new branch

--- server.js
+++ server.js
@@ -1,145 +1,52 @@
-// const inquirer = require('inquirer');
-// const fs = require('fs');
-// const mysql = require('mysql2');            // Import and require mysql2
-
-// // Connect to database
-// const db = mysql.createConnection(
-//     {
-//       // MySQL username,
-//       user: 'root',
-//       // TODO: Add MySQL password here
-//       password: 'C0d!ng',
-//       database: 'company_db'
-//     },
-//     console.log(`Connected to the company_db database.`)
-//   );
-
-// // ----- INQUIRER PROMPTS ----- //
-// const actionQuestions = [            
-//     {
-//     type: 'list',
-//     message: 'What would you like to do?',
-//     name: 'actionQuestion',
-//     choices: ['View All Employees','Add Employee','Update Employee Role',
-//     'View All Roles','Add ROle','View All Departments','Add Department','Quit'],
-//     },
-// ];
-
-// const addDepartmentQuestion = [
-//     {
-//         type: 'input',
-//         message: 'What is the name of the department?',
-//         name: 'departmentName',
-//     },
-// ];
-
-// const addRoleQuestion = [
-//     {
-//         type: 'input',
-//         message: 'What is the name of the role?',
-//         name: 'roleName',
-//     },
-//     {
-//         type: 'input',
-//         message: 'What is the salary of the role?',
-//         name: 'roleSalary',
-//     },
-// ];
+const inquirer = require('inquirer');
+const fs = require('fs');
+const mysql = require('mysql2');            // Import and require mysql2
+const cTable = require('console.table');    // Import and require console.table
+const { Console } = require('console');
 
 
 
-// const updateEmployeeRole = [            
-//     // {
-//     // type: 'list',
-//     // message: "Which employee's role would you like to update?",
-//     // name: 'employeeRoleUpdate',
-//     // choices: 
-//     // },
-//     {
-//         type: 'list',
-//         message: "Which role would you like assigned to the selected employee?",
-//         name: 'employeeRoleAssignment',
-//         choices: roles,
-//         },
-// ];
+const addEmployeeQuestions = [
+    {
+        type: 'input',
+        message: 'What is the employees first name?',
+        name: 'employeeFirstName',
+    },
+    {
+        type: 'input',
+        message: 'What is the employees last name?',
+        name: 'employeeLastName',
+    },
+  ];
 
+function inquirerFunction(questions){
+    let questionName=[];
+    let prompt = [];
+    for (let i=0;i<questions.length;i++){
+        questionName[i]= [questions[i].name];
+        console.log(questionName)
+    }
 
-// // ----- CATAGORIES FOR PROMPT QUESTIONS ----- //
-// // const departments = [
-// //     'Engineering', 'Finance', 'Legal', 'Sales', 'Service'
-// // ];
+    inquirer
+    .prompt(questions)
+    .then((data) => {
+      for (let i=0; i<questionName.length; i++){
+        prompt[i] = index(data,questionName[i])
+      }
+    //   console.log(prompt)
+  });
+    return prompt
+  }
 
-// const employeeRole = [
-//     'Sales Lead','Salesperson', 'Lead Engineer', 
-//     'Software Engineer', 'Account Manager', 'Accountant', 
-//     'Legal Team Lead', 'Lawyer', 'Customer Service'
-// ];
+  inquirerFunction(addEmployeeQuestions)
 
-// const employeeManagers = [
-//     'None','Fred'
-// ];
-
-// const employees = [
-//     'None','Fred'
-// ];
-
-// // Creates an array of questions for user input
-// const questions = [
-//             {
-//                 type: 'list',
-//                 message: 'What would you like to do?',
-//                 name: 'actionQuestion',
-//                 choices: actionQuestions,
-//             },
-//             {
-//                 type: 'input',
-//                 message: 'Contributing:',
-//                 name: 'contributing',
-//                 default: 'N/A',
-//             },
-//         ];
-
-// // // Creates a function to write README file
-// // function writeToFile(fileName, data) {
-
-// //     const md = generateMarkdown(data)
-// //     fs.writeFile("README.md",md, (err) => {
-// //         if (err) {
-// //             console.error(err)
-// //         } else {
-// //             console.log('Success: README.md File Generated!')
-// //         }
-// //     })
-// // }
-
-// // // Creates a function to initialize app
-// // function init() {
-// //     inquirer
-// //         .prompt(questions)
-// //         .then((data) => {
-// //             const prompt = {
-// //                 title: data.title,
-// //                 description: data.description,
-// //                 installation: data.installation,
-// //                 usage: data.usage,
-// //                 license: data.license,
-// //                 contributing: data.contributing,
-// //                 test: data.test,
-// //                 username: data.username,
-// //                 email: data.email
-// //             }
-// let prompt;
-// let Name;
- 
-// for (let i=0; i<data.length;i++){
-//   Name = questions[0].name;
-//   console.log(Name)
-//   prompt[i]= data.Name;
-//   console.log(prompt)
-// }
-// //         writeToFile(prompt.title,prompt)
-// // });
-// // }
-
-// // // Function call to initialize app
-// // init();
+  function index(obj,is, value) {
+    if (typeof is == 'string')
+        return index(obj,is.split('.'), value);
+    else if (is.length==1 && value!==undefined)
+        return obj[is[0]] = value;
+    else if (is.length==0)
+        return obj;
+    else
+        return index(obj[is[0]],is.slice(1), value);
+}
