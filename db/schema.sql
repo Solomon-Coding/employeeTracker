e7685795f7b395c537cd5423f64ffcaa8eccd79d
Solomon Vana <117047239+Solomon-Coding@users.noreply.github.com>
2023-02-26 23:28:39 -0600

    Added method for "addeding" a department to the sql table

--- db/schema.sql
+++ db/schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,              
     last_name VARCHAR(30) NOT NULL,               
     role_id INT,
@@ -26,35 +26,3 @@ CREATE TABLE employee (
     FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL,                 
     FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL   
 );
-
-
--- CREATE TABLE employee (
---     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     first_name VARCHAR(30) NOT NULL,              
---     last_name VARCHAR(30) NOT NULL,               
---     role_id INT,
---     department_id INT,                       
---     manager_id INT,  
---     FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL                  
---     FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,         
---     FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
--- );
--- DROP DATABASE IF EXISTS movies_db;
--- CREATE DATABASE movies_db;
-
--- USE movies_db;
-
--- CREATE TABLE movies (
---   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---   movie_name VARCHAR(100) NOT NULL
--- );
-
--- CREATE TABLE reviews (
---     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     movie_id INT,
---     review TEXT NOT NULL,
---     FOREIGN KEY (movie_id)
---     REFERENCES movies(id)
---     ON DELETE SET NULL
--- );
-
