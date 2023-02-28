033e52c68b64cc4b66429dcdb3489bd919075feb
Solomon Vana <117047239+Solomon-Coding@users.noreply.github.com>
2023-02-27 23:55:39 -0600

    Updated README and polished up the code.

--- db/query.sql
+++ db/query.sql
@@ -1,3 +1,10 @@
-SELECT role.id, role.title
+SELECT employee.role_id 
+FROM employee
+WHERE department_id = VAL
+
+
+SELECT SUM(role.salary)
 FROM role
-ORDER BY role.id
+WHERE role.id = employee.role_id IN (SELECT employee.role_id 
+                                    FROM employee
+                                    WHERE department_id = VAL)
--- db/seeds.sql
+++ db/seeds.sql
@@ -16,5 +16,7 @@ VALUES (1, "Sales Lead", 100000, 4),
        (8, "Lawyer", 190000, 3);
 
 INSERT INTO employee (id, first_name, last_name, role_id, department_id, manager_id)
-VALUES (1, "Fred", "Bob", 2, 4, NULL),
-       (2, "George", "Sal", 3, 1, 1)
+VALUES (1, "Fred", "Bob", 2, 4, 1),
+       (2, "George", "Sal", 3, 1, 1),
+       (3, "Sally", "Johnson", 8, 3, 1),
+       (4, "Faith", "Rooney", 4, 1, 2);
